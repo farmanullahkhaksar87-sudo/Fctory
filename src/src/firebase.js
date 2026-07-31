d3ef445435148cf3d4fb7f708bc59d17f93f0fd4
farmanullahkhaksar87-sudo <farmanullahkhaksar87@gmail.com>
2026-07-31 18:56:45 +0500

    Add Firebase initialization and Firestore setup
    Initialized Firebase app with Firestore configuration.

---
+++ src/src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// آپ کے فائر بیس پروجیکٹ (factory-b43a7) کی کنفیگریشن
+const firebaseConfig = {
+  apiKey: "AIzaSyBC_hHb1MZTUy-9zjMjrQx-Fq7aWOoUcN0",
+  authDomain: "factory-b43a7.firebaseapp.com",
+  projectId: "factory-b43a7",
+  storageBucket: "factory-b43a7.firebasestorage.app",
+  messagingSenderId: "759335174409",
+  appId: "1:759335174409:web:3d5a758a89498cb23013d8"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
